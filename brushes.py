96cb24ff1ad490ebadaa0e5b09e006c65bb36087
naahian <nahiandev@gmail.com>
2024-05-01 20:32:49 +0600

    added comment

--- brushes.py
+++ brushes.py
@@ -1,27 +1,23 @@
 import pygame
 from constants import Colors
 
-
 class Brush:
     sizes = [5, 15, 25]
 
     def __init__(self, screen):
         self.size = Brush.sizes[0]
-        self.color = (0,0,0)
+        self.color = Colors.black
         self.screen = screen
 
     def handleEvents(self):
-        keysPressed = pygame.key.get_pressed()        
-        if(pygame.mouse.get_pressed()[0]):
+        if(pygame.mouse.get_pressed()[0]):  #if left mouse button pressed
             self.draw()
-
 
     def setBrushSize(self, size):
         self.size = size
     
     def setColor(self, color):
         self.color = color
-    
     
     def draw(self):
         position = pygame.mouse.get_pos()
